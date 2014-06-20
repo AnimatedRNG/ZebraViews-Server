c12dc56be240e9b56aed9bcf570aa649b8a5532f
AnimatedJuzz <kazasrinivas3@gmail.com>
2014-06-19 19:07:18 -0500

    Added interpreter class to handle server or client commands

---
+++ src/zebradev/zebraviews/server/CommandInterpreter.java
@@ -0,0 +1,38 @@
+package zebradev.zebraviews.server;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyledDocument;
+
+public class CommandInterpreter {
+
+	private StyledDocument doc;
+	private BufferedWriter log;
+	
+	public static final String START_RESPONSE = "Starting server....";
+	
+	public CommandInterpreter(StyledDocument doc, BufferedWriter log) {
+		this.doc = doc;
+		this.log = log;
+	}
+	
+	public void interpret(String input) throws BadLocationException, IOException {
+		Style style = doc.getStyle("regular");
+		if (input.charAt(1) == 's')
+		{
+			String command = input.substring(8);
+			if (command.equals(ServerCommands.START.toString()))
+			{
+				ServerRunner.log(this.doc, this.log, START_RESPONSE, "regular");
+			}
+			// Handle other possible responses in if/else blocks
+		}
+		else
+		{
+			// Fake client handling
+		}
+	}
+}
