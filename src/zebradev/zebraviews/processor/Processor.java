0a42ae9d5ea8a7067fda5ba57c6302d04f58846d
AnimatedJuzz <kazasrinivas3@gmail.com>
2014-06-22 10:16:46 -0500

    Processor abstract class (for future use)

---
+++ src/zebradev/zebraviews/processor/Processor.java
@@ -0,0 +1,45 @@
+//	This file is part of ZebraViews.
+//
+//	Copyright 2014 AnimatedJuzz <kazasrinivas3@gmail.com>
+//
+//	ZebraViews is free software: you can redistribute it and/or modify
+//	under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	ZebraViews is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with ZebraViews.  If not, see <http://www.gnu.org/licenses/>.
+
+package zebradev.zebraviews.processor;
+
+import java.util.TreeMap;
+
+import com.esotericsoftware.minlog.Log;
+
+public abstract class Processor implements Runnable {
+	
+	private TreeMap<String, Object> product;
+	
+	@Override
+	public void run() {
+		if (this.product != null)
+			this.onExecute(this.product);
+		else
+			Log.error("Product object not supplied!");
+	}
+	
+	public TreeMap<String, Object> getProduct() {
+		return this.product;
+	}
+	
+	public void setProduct(TreeMap<String, Object> product) {
+		this.product = product;
+	}
+	
+	protected abstract void onExecute(TreeMap<String, Object> product);
+}
