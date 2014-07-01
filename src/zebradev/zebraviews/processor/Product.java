1e2b4d48234e61cb790e709cbf37a40b569a2ac7
AnimatedJuzz <kazasrinivas3@gmail.com>
2014-07-01 18:08:12 -0500

    Added new synchronized methods for getting and setting attributes in the "basic info" TreeMap

--- src/zebradev/zebraviews/processor/Product.java
+++ src/zebradev/zebraviews/processor/Product.java
@@ -7,6 +7,18 @@ public class Product extends ConcurrentLinkedQueue<TreeMap<String, Object>> {
 
 	private static final long serialVersionUID = -7183752795726987983L;
 
+	public Product() {
+		this.add(new TreeMap<String, Object>());
+	}
+	
+	public synchronized void putTop(String attributeName, Object object) {
+		this.peek().put(attributeName, object);
+	}
+	
+	public synchronized Object getTop(String attributeName) {
+		return this.peek().get(attributeName);
+	}
+	
 	// Intensive process -- complete in separate thread
 	@SuppressWarnings("unchecked")
 	public TreeMap<String, Object> concatenate() {
